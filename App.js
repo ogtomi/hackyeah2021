5f46922b3d287b8a9a56fc6150fdd4bf155fc776
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2021-12-10 18:23:48 +0100

    cleaned

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text } from 'react-native'
 
-import App from './src';
-
-export default App;
+export default function App() {
+  return (
+    <View>
+      <Text>
+        APP
+      </Text>
+    </View>
+  );
+}
